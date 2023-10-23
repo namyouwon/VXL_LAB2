629209baadee1d4586f2ce39030b331d853d20ed
Nam <hoainam15112002@gmail.com>
2023-10-23 09:59:28 +0700

    update

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -86,6 +86,25 @@ void update7SEG ( int index ) {
 			 break ;
  	 }
  }
+int hour = 15 , minute = 8 , second = 50;
+void updateClockBuffer (){
+	if(hour<10){
+		led_buffer[0]=0;
+		led_buffer[1]=hour;
+	}
+	else{
+		led_buffer[0]=hour/10;
+		led_buffer[1]=hour%10;
+	}
+	if(minute<10){
+		led_buffer[2]=0;
+		led_buffer[3]=minute;
+	}
+	else{
+		led_buffer[2]=minute/10;
+		led_buffer[3]=minute%10;
+	}
+}
 /* USER CODE END 0 */
 
 /**
@@ -123,24 +142,23 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  setTimer1(25);
-  setTimer2(100);
-  init7SEG();
+
   while (1)
   {
-	if(timer2_flag){
-		HAL_GPIO_TogglePin(DOT_GPIO_Port, DOT_Pin);
-		setTimer2(100);
-	}
-	if(timer1_flag){
-		if(index_led>3){
-			index_led=0;
-		}
-
-		update7SEG(index_led);
-		index_led++;
-		setTimer1(25);
-	}
+	  second ++;
+	  if ( second >= 60) {
+		  second = 0;
+		  minute ++;
+	  }
+	  if( minute >= 60) {
+		  minute = 0;
+		  hour ++;
+	  }
+	  if( hour >=24) {
+		  hour = 0;
+	  }
+	  updateClockBuffer () ;
+	  HAL_Delay (1000) ;
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
