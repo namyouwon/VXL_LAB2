154bc3923f2b5ef09463e0e670d799d7bb6c23dd
Nam <hoainam15112002@gmail.com>
2023-10-26 02:56:19 +0700

    update

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -59,7 +59,7 @@ static void MX_GPIO_Init(void);
 /* USER CODE BEGIN 0 */
 const int MAX_LED = 4;
 int index_led = 0;
-int led_buffer [4] = {1 , 2 , 3 , 4};
+int led_buffer [4] = {1 , 2 , 3 , 0};
 void update7SEG ( int index ) {
 	 switch ( index ) {
 		 case 0:
